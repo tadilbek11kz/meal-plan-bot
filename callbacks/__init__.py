1fadff5ed8d3b92cd8d0ccfe9658584311bde8da
tadilbek11kz <tadilbek11kz@gmail.com>
2021-11-10 18:22:56 +0400

    feat: create callback for image

--- callbacks/__init__.py
+++ callbacks/__init__.py
@@ -2,4 +2,4 @@ from aiogram.utils.callback_data import CallbackData
 
 subscription_callback = CallbackData("subscription", "command")
 get_data_callback = CallbackData("get_data", "type")
-image_callback = CallbackData("image")
+image_callback = CallbackData("image", "value")
